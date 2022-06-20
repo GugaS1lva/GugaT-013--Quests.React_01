973d76f65b2e46de7cfb3c5f9aca24aec624ae31
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-06-20 06:58:22 -0300

    Fim do Desafio.

--- pages/components/textstyler/textstyler.js
+++ pages/components/textstyler/textstyler.js
@@ -1,5 +1,5 @@
 function TextStyler({children}){
-    const insertedText = children
+    const insertedText = children.toUpperCase()
 
     return <div style={{color: 'red'}}>{insertedText}</div>
 }
